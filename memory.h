2fe27e748f5d419616bd91d7c1c3a0e28a83e844
dengarIicdk <danya.belov.2007@internet.ru>
2026-03-28 02:50:58 +0300

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,22 +1,90 @@
 #pragma once
-#include <windows.h>
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <TlHelp32.h>
+
 #include <cstdint>
-#pragma comment(lib, "user32.lib")
+#include <string_view>
 
-// ПРЯМЫЕ ПУТИ К ДАМПЕРУ
-#include "Z:/icdk_e/dumper/output/client_dll.hpp"
-#include "Z:/icdk_e/dumper/output/offsets.hpp"
+class Memory
+{
+private:
+	std::uintptr_t processId = 0;
+	void* processHandle = nullptr;
 
-namespace memory {
-    template <typename T>
-    static T Read(uintptr_t address) {
-        if (!address) return T();
-        return *reinterpret_cast<T*>(address);
-    }
+public:
 
-    template <typename T>
-    static void Write(uintptr_t address, T value) {
-        if (!address) return;
-        *reinterpret_cast<T*>(address) = value;
-    }
-}
+	Memory(const std::string_view processName) noexcept
+	{
+		::PROCESSENTRY32 entry = { };
+		entry.dwSize = sizeof(::PROCESSENTRY32);
+
+		const auto snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+		while (::Process32Next(snapShot, &entry))
+		{
+
+			char exeFileA[MAX_PATH];
+			::WideCharToMultiByte(CP_ACP, 0, entry.szExeFile, -1, exeFileA, MAX_PATH, NULL, NULL);
+
+			if (!processName.compare(exeFileA))
+			{
+				processId = entry.th32ProcessID;
+				processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+				break;
+			}
+		}
+
+		if (snapShot)
+			::CloseHandle(snapShot);
+	}
+
+	~Memory()
+	{
+		if (processHandle)
+			::CloseHandle(processHandle);
+	}
+
+	const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
+	{
+		::MODULEENTRY32 entry = { };
+		entry.dwSize = sizeof(::MODULEENTRY32);
+
+		const auto snapShot = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, processId);
+
+		std::uintptr_t result = 0;
+
+		while (::Module32Next(snapShot, &entry))
+		{
+
+			char moduleNameA[MAX_PATH];
+			::WideCharToMultiByte(CP_ACP, 0, entry.szModule, -1, moduleNameA, MAX_PATH, NULL, NULL);
+
+			if (!moduleName.compare(moduleNameA))
+			{
+				result = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				break;
+			}
+		}
+
+		if (snapShot)
+			::CloseHandle(snapShot);
+
+		return result;
+	}
+
+	template <typename T>
+	constexpr const T Read(const std::uintptr_t& address) const noexcept
+	{
+		T value = { };
+		::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
+		return value;
+	}
+
+	template <typename T>
+	constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
+	{
+		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
+	}
+};
